c8deab45db16c6ca769777bbd5d269684e809e0b
Yunus Emre Karaoğlan <karaoglan18@itu.edu.tr>
2022-04-29 22:03:22 +0300

    declared classes, constructors and destructors

--- object.cpp
+++ object.cpp
@@ -1,1 +1,111 @@
+#include "object.h"
+Object::Object()
+{
+    std::cout << "Object created" << std::endl;
+}
+
+Object::~Object()
+{
+    std::cout << "Object destroyed" << std::endl;
+}
+
+Wire::Wire()
+{
+    std::cout << "Wire created" << std::endl;
+}
+
+Wire::~Wire()
+{
+    std::cout << "Wire destroyed" << std::endl;
+}
+
+Pin::Pin()
+{
+    std::cout << "Pin created" << std::endl;
+}
+
+Pin::~Pin()
+{
+    std::cout << "Pin destroyed" << std::endl;
+}
+
+LogicElement::LogicElement()
+{
+    std::cout << "LogicElement created" << std::endl;
+}
+
+LogicElement::LogicElement(int numPins){
+    this->numPins = numPins;
+}
+
+LogicElement::~LogicElement()
+{
+    std::cout << "LogicElement destroyed" << std::endl;
+}
+
+AndGate::AndGate(): LogicElement(3){
+    std::cout << "AndGate created" << std::endl;
+}
+
+AndGate::~AndGate()
+{
+    std::cout << "AndGate destroyed" << std::endl;
+}
+
+OrGate::OrGate() : LogicElement(3)
+{
+    std::cout << "OrGate created" << std::endl;
+}
+
+OrGate::~OrGate()
+{
+    std::cout << "OrGate destroyed" << std::endl;
+}
+
+XorGate::XorGate() : LogicElement(3)
+{
+    std::cout << "XorGate created" << std::endl;
+}
+
+XorGate::~XorGate()
+{
+    std::cout << "XorGate destroyed" << std::endl;
+}
+
+NotGate::NotGate() : LogicElement(2)
+{
+    std::cout << "NotGate created" << std::endl;
+}
+
+NotGate::~NotGate()
+{
+    std::cout << "NotGate destroyed" << std::endl;
+}
+
+
+DFlipFlop::DFlipFlop() : LogicElement(4)
+{
+    std::cout << "DFlipFlop created" << std::endl;
+}
+
+DFlipFlop::~DFlipFlop()
+{
+}
+
+LogicOne::LogicOne(): LogicElement(1)
+{
+    std::cout << "LogicOne created" << std::endl;
+}
+{
+    std::cout << "LogicOne created" << std::endl;
+}
+
+LogicOne::~LogicOne()
+{
+}
+
+OrGate::OrGate()
+{
+    std::cout << "OrGate created" << std::endl;
+}
 
--- object.h
+++ object.h
@@ -1,6 +1,7 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+
 class Object { // absract  // derived from logic gates
     protected:
     Object* next;
@@ -9,6 +10,8 @@ class Object { // absract  // derived from logic gates
     bool selected;
 
     public:
+    Object();
+    ~Object();
     sf::RenderWindow* window;
     sf::Texture texture;
     sf::Sprite sprite;
@@ -21,6 +24,8 @@ class Wire : public Object {
     private:
     Pin* pins[2];
     public:
+    Wire();
+    ~Wire();
     sf::Vertex line [2];
     
 }
@@ -32,60 +37,110 @@ class Pin {
     
     int index;
     
-
     public:
-    
+    Pin();
+    ~Pin();
+
+    pinType type;
+    bool isSrc[MAX_CONNECTIONS];
+
+    Pin* conntectedTo[MAX_CONNECTIONS];
+    Object* wires[MAX_CONNECTIONS];
+    int numConnections;
+    sf::Vector2f pos;
+    pinState state;
+
 
 };
 
 class LogicElement : public Object {
     Pin pins[4];
     int numPins;
+    public:
+    LogicElement();
+    LogicElement(int);
+    ~LogicElement();
+    virtual void simulate() = 0;
 };
 
 class AndGate: public LogicElement{
-    
+    public:
+    AndGate();
+    AndGate(int);
+    ~AndGate();
+    private:
+    void simulate();
     
 };
 
 class OrGate: public LogicElement{
-
+    public:
+    OrGate();
+    ~Orgate();
+    private:
+    void simulate();
     
 };
 
 class XorGate: public LogicElement{
-
-    
+    public:
+    XorGate();
+    ~XorGate();
+    private:
+    void simulate();
 };
 
 class NotGate: public LogicElement{
-
-    
+    public:
+    NotGate();
+    ~NotGate();
+    private:
+    void simulate();
 };
 
 class DFlipFlop: public LogicElement{
-
-    
+    public:
+    DFlipFlop();
+    ~DFlipFlop();
+    private:
+    void simulate();
 };
 
 class LogicOne: public LogicElement{
-
-    
+    public:
+    LogicOne();
+    ~LogicOne();
+    private:
+    void simulate();
 };
 
 class Gnd: public LogicElement{
-
-    
+    public:
+    Gnd()
+    private:
+    void simulate();
 };
 
 class Clock: public LogicElement{
+    public:
+    Clock()
+    private:
     sf::Clock clock;
-    
+    void simulate();
 };
 
 class LED: public LogicElement{
-
+    public:
+    LED()
+    private:
+    void simulate();
     
+};
+
+class Simulator {
+    public:
+    Simulator();
+
 };
 
 #endif // !OBJECT_H
